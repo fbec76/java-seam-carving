00aebfacb3c601a6242763143a294d44ad46b17e
Finn Beckmann <fbeckmann.edu@pm.me>
2023-02-07 21:32:50 +0100

    Revert "calculate energy"
    This reverts commit f1222aa83133b9fe26b60969af66b936e9491915.

--- seamcarving-app/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module de.fbeckmann.seamcarvingapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-
-    opens de.fbeckmann.seamcarvingapp to javafx.fxml;
-    exports de.fbeckmann.seamcarvingapp;
-}
